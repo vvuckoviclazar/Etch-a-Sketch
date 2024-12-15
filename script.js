8bb749a7cb5677d0c483f3e710de60c2905d0082
Lazar Vuckovic <lazarvuckovic777@gmail.com>
2024-12-15 12:16:35 +0100

    HTML and CSS

---
+++ script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+//how to create 16x16 grid with javascrpt
+//"mouseover"
+//input type="color" (slusaj na event "input" ili "change")
+// od varijabli u kreatorsku funkciju staviti currentColor i currentMode
+// random boju uraditi zadnju
+// how to generate random color with javaScript
+// element.style.backgroundcolor = "rgb(255, 0, 0)";
